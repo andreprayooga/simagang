2d8c0e8ccf3cd401d9f3b91c59a231cb5302963b
tholibsubechan <tholibsubechan@gmail.com>
2019-01-31 11:37:46 +0700

    tambah login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,64 +1,69 @@
-<?php
+<?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-class login extends CI_Controller {
-
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->library('form_validation', 'session');
-	}
-
-	public function index()
-	{
-		$this->load->view('auth/login');
-	}
-
-	public function login(){
-
-		$this->load->model('AdminModel');
-        $this->form_validation->set_rules('username', 'Username', 'required');
-        $this->form_validation->set_rules('password', 'Password', 'required');
-        if($this->form_validation->run() === FALSE){
-            $this->load->view('auth/login');
+class Login extends CI_Controller {
+    public function index()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('username','Username','required|trim|callback_cekDB');
+        $this->form_validation->set_rules('password','Password','required');
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('user/login');
         } else {
-            // Get username
-    $username = $this->input->post('username');
-    // Get & encrypt password
-    $password = $this->input->post('password');
-    // Login user
-	 $id_administrator = $this->AdminModel->login($username, $password);
-
-    if($id_administrator){
-        // Buat sessio n
-        $user_data = array(
-            'id_administrator' => $id_administrator,
-            'username' => $username,
-            'fk_id_level' => $id_administrator['fk_id_level'],
-            'logged_in' => true
-        );
-         $this->session->set_userdata($user_data);
-         $this->session->set_flashdata('user_loggedin', 'You are now logged in');
-            if ($id_administrator['fk_id_level'] == '1') {
-                    redirect('Admin/Sekolah/index');
-            } else {
-        // Set message
-                $this->session->set_flashdata('login_failed', 'Login is invalid');
-                redirect('Login/index', 'refresh');
-				}
-			}
-		  }
-		}
-
-		public function logout(){
-        // Unset user data
-        $this->session->unset_userdata('logged_in');
-        $this->session->unset_userdata('id_administrator');
-        $this->session->unset_userdata('username');
-        // Set message
-        $this->session->set_flashdata('user_loggedout', 'Anda sudah log out');
-        redirect('Home');
+            redirect('Home','refresh');
+        }
     }
-
-
+    public function cekDB($username)
+    {
+        $password = $this->input->post('password');
+        $cekDB = $this->db->where(array('username'=>$username,'password'=>$password))->get('siswa_magang');
+        if ($cekDB->num_rows() == 1) {
+            $data = $cekDB->result()[0];
+            $userdata = array(
+                'id' => $data->id,
+                'nama' => $data->nama,
+                'username' => $username
+            );
+            $this->session->set_userdata('logged_in_user',$userdata);
+            return true;
+        }else{
+            $this->form_validation->set_message('cekDB','Username dan password tidak valid');
+            return false;
+        }
+    }
+    public function admin()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('username','Username','required|trim|callback_cekDB_admin');
+        $this->form_validation->set_rules('password','Password','required');
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('admin/login');
+        } else {
+            redirect('Admin/Dashboard','refresh');
+        }
+    }
+    public function cekDB_admin($username)
+    {
+        $password = $this->input->post('password');
+        $cekDB_admin = $this->db->where(array('username'=>$username,'password'=>$password))->get('administrator');
+        if ($cekDB_admin->num_rows() == 1) {
+            $data = $cekDB_admin->result()[0];
+            $userdata = array(
+                'id' => $data->id,
+                'nama' => $data->nama,
+                'username' => $username,
+                'fk_level' => $data->fk_level
+            );
+            $this->session->set_userdata('logged_in',$userdata);
+            return true;
+        }else{
+            $this->form_validation->set_message('cekDB_admin','Username dan password tidak valid');
+            return false;
+        }
+    }
+    public function logout()
+    {
+        $this->session->unset_userdata('logged_in');
+        $this->session->unset_userdata('logged_in_user');
+        redirect('Home','refresh');
+    }
 }
